d308cce57e1430ddc5a57173f4f9897e861d6dbd
Delete Prod <martynjamesoconnor@hotmail.co.uk>
2024-08-28 16:26:44 +0100

    Moved VPC resource out of main.tf and into vpc.tf

--- main.tf
+++ main.tf
@@ -11,15 +11,6 @@ provider "aws" {
   region = var.region_map["dublin"]
 }
 
-# Start building - VPC
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    name = "Martian VPC"
-  }
-}
-
 
 # Start building - EC2
 resource "aws_instance" "phobos" {
---
+++ vpc.tf
@@ -0,0 +1,8 @@
+# Start building - VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    name = "Martian VPC"
+  }
+}
